fce4547dde2c3aa0abd47baf7f9dea2807291619
mcbayrak <miratcanbayrak@gmail.com>
2011-05-30 02:25:53 +0300

    backuping started...

--- ffsnake/ff_user_counter.py
+++ ffsnake/ff_user_counter.py
@@ -1,34 +1,51 @@
 from api.friendfeed import FriendFeed
 from api.friendfeed import urllib2
+from os.path import exists
+import cPickle
 
-# start from here
-starting_feeds = ("debbieturner",)
-# stack
+
+starting_feeds = ("aynebilm", "5posta", "joanmiro")
+
 feed_stack = set(starting_feeds)
-# repo
-feed_repo = set()
-# api
+feed_repo  = set()
 ff_api = FriendFeed()
 
-def get_subscribers(feed_id):
-    print "Extending stack with subscribers of", feed_id
+
+def subscribers_of(feed_id):
     try:
         feed_info = ff_api.fetch_feed_info(feed_id)
     except urllib2.HTTPError:
-        print feed_id, "is not exists, skipping."
+        feed_info = None
+        print "Skipping :", feed_id 
+    except:
+        cPickle.dump(feed_stack, file("feed_stack.pkl", "w"))
+        cPickle.dump(feed_repo , file("feed_repo.pkl",  "w"))
 
     if feed_info:
         subscribers = set([subscription["id"] for subscription in feed_info['subscribers']])
     else:
         subscribers = set()
+
     return subscribers
 
+# if there is saved process, load it.
+if exists("feed_stack.pkl"):
+    feed_stack = cPickle.load(file("feed_stack.pkl"))
 
-while stack:
+if exists("feed_repo.pkl"):
+    feed_repo = cPickle.load(file("feed_repo.pkl"))
+
+while feed_stack:
+    #get current feed
     current_feed = feed_stack.pop()
-    current_feeds_subscribers = get_subscribers(current_feed)
-    feed_stack.update(current_feeds_subscribers)
-    feed_repo.update(current_feeds_subscribers)
-    print "Current stack size : %s, Feed repo size : %s " % (len(feed_stack), len(feed_repo))
-
-print stack
+    # add it to repo
+    feed_repo.add(current_feed)
+    # get subscribers of current feed
+    subscribers = subscribers_of(current_feed)
+    # differ subscribers from feed_repo and feed_stack
+    # only feed_stack.update is not enought
+    subscribers.difference_update(feed_repo.union(feed_stack))
+    # push subscribers to feed_stack
+    feed_stack.update(subscribers)
+    # give some report bro
+    print "Stack merged with %s's %d subscribers. Stack size: %s, Repo size: %s " % (current_feed, len(subscribers), len(feed_stack), len(feed_repo))
